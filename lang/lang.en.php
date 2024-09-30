3a9c6ca0b1245281a84eaa93e401ae5e69d446fa
Christopher Isene <christopher.isene@gmail.com>
2024-09-30 15:32:24 +0200

    Updated language files en + sv

--- lang/lang.en.php
+++ lang/lang.en.php
@@ -4,3 +4,11 @@
 
 $lang['language_name'] = 'English';
 
+$lang['database_hostname']  = 'Database hostname';
+$lang['database_port']      = 'Database port';
+$lang['database_username']  = 'Database username';
+$lang['database_password']  = 'Database password';
+$lang['database_database']  = 'Database database name';
+
+
+
--- lang/lang.sv.php
+++ lang/lang.sv.php
@@ -4,3 +4,11 @@
 
 $lang['language_name'] = 'Svenska';
 
+$lang['database_hostname']  = 'Databas värdnamn';
+$lang['database_port']      = 'Databas port';
+$lang['database_username']  = 'Databas användarnamn';
+$lang['database_password']  = 'Databas lösenord';
+$lang['database_database']  = 'Databas databas-name';
+
+
+
