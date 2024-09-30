901e71d48827839fcd1926b4a0fa51e77cca63c8
Christopher Isene <christopher.isene@gmail.com>
2024-09-30 14:57:59 +0200

    Stub index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors','On');
+
+// Load config
+include_once "config.php";
+
+// Load LM
+include_once "class.LanguageManager.php";
+
+$lm = new LanguageManager($config);
+
+// Start a Session, You might start this somewhere else already.
+session_start();
+
+
+// Include active language
+include('lang/lang.' . $_SESSION['lang'] . '.php');
+
